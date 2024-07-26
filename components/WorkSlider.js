f4d466b09ef9473856cb9d3bf2d7ed7a44911c19
Eduardopereira11967484018 <eduardoperira.lima@yahoo.com.br>
2024-07-26 10:35:10 -0300

    configuraçao das imagens

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -74,9 +74,21 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-                  <a key={index} href={image.link} target="_blank" rel="noopener noreferrer" className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+                  <a
+                    key={index}
+                    href={image.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                  >
                     <div className="flex items-center justify-center relative overflow-hidden group">
-                      <Image src={image.path} width={500} height={300} alt="" />
+                      <Image
+                        src={image.path}
+                        width={500}
+                        height={300}
+                        alt={image.title}
+                        className="object-cover w-full h-full"
+                      />
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-5 group-hover:xl:-translate-y-20 transition-all duration-300">
                         <div className="flex flex-col xl:flex-row items-center gap-x-2 text-xs xl:text-[13px] tracking-[0.2em]">
