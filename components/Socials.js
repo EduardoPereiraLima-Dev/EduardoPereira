b909bb3d0d8917399b81da022e4273e17226d93e
Eduardo Pereira Lima <140276300+EduardoPereiraLima-Dev@users.noreply.github.com>
2025-05-12 11:48:51 -0300

    Update Socials.js
    alterando o link do github

--- components/Socials.js
+++ components/Socials.js
@@ -15,7 +15,7 @@ import {
 
 const Socials = () => {
   return <div className='flex items-center gap-5 text-lg'>
-    <Link href={'https://github.com/Eduardopereira11967484018'} className='hover:text-accent transition-all duration-300'>
+    <Link href={'https://github.com/EduardoPereiraLima-Dev'} className='hover:text-accent transition-all duration-300'>
       <RiGithubLine />
     </Link>
     <Link href={'https://www.linkedin.com/in/eduardo-pereira-lima-7580a5239'} className='hover:text-accent transition-all duration-300'>
